ae5206a950e8a9a80c450f6ac0e2b494cbe6ee3e
Benito <bbxblack@gmail.com>
2021-06-02 16:43:34 +0200

    v0.4

---
+++ MyLaravelOspiti/resources/views/pages/create.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.main-layout')
+
+@section('principal')
+
+    <h1>NUOVO OSPITE </h1>
+    
+    <form method="POST" action="{{ route('store') }}">
+        
+        @csrf
+        @method('POST')
+
+        
+        <div class="row">
+            <label for="name" class="inp-wrk">name:</label>
+            <input type="text" name="name" class="inp-wrk">
+        </div>
+        
+        <div class="row">
+            <label for="lastname" class="inp-wrk">lastname:</label>
+            <input type="text" name="lastname" class="inp-wrk">
+        </div>
+        
+        <div class="row">
+            <label for="date_of_birth" class="inp-wrk">giorno di nascita:</label>
+            <input type="date" name="date_of_birth" class="inp-wrk">
+        </div>
+        
+        <div class="row">
+            <label for="document_type" class="inp-wrk">tipo di documento:</label>
+            <input type="text" name="document_type" class="inp-wrk">
+        </div>
+
+        <div class="row">
+            <label for="document_number" class="inp-wrk">numero di documento:</label>
+            <input type="number" name="document_number" class="inp-wrk">
+        </div>
+
+        <div class="row">
+            <button class="bnt">
+                CREATE
+            </button>
+        </div>  
+    </form>
+
+
+@endsection
